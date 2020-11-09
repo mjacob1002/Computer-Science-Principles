98d40b7eae94c3e840a0c4792a263a8b81fbeba4
Mathew Jacob <63362973+mjacob1002@users.noreply.github.com>
2020-11-09 11:38:18 -0500

    Added Comments for Dr. LaRocca

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -16,18 +16,22 @@ special = {
 def gen_password(site: str):
     # reverse the string
     password = site[::-1]
+    # get length of site
     size = len(site) % 10
+    # add the special character to end of reversed site
     password = password + special[size]
+    # add the length of the site to the end of the string
     password = password + str(size)
     return password
 
 
 def prompt():
     print("What is the website that your password is for?")
+    # get the site password is generated for
     site = input()
     return site
 
-
+# function isn't used anymore, but good practice for Dynamic Programming
 def __factorial__(n: int):
     nums = [1]
     a: int
